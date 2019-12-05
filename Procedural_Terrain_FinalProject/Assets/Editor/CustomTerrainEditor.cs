8a7b8442a962410770634f8dd77a0eaa3337d892
Nick <dacosta.nick@yahoo.com>
2019-12-04 23:53:19 -0500

    Added initial unity project
    Started procedural terrain generation udemy tutorial.
    Progress up to and including section 2.10

---
+++ Procedural_Terrain_FinalProject/Assets/Editor/CustomTerrainEditor.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEditor;
+using EditorGUITable;   // Download from asset store. $10
+
+// Link this script with another script.
+[CustomEditor(typeof(CustomTerrain))]
+[CanEditMultipleObjects]
+
+public class CustomTerrainEditor : Editor
+{
+    private void OnEnable()
+    {
+
+
+        return;
+    }
+
+    public override void OnInspectorGUI()
+    {
+
+
+        return;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+        return;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        return;
+    }
+}
---
+++ Procedural_Terrain_FinalProject/Assets/Scripts/CustomTerrain.cs
@@ -0,0 +1,62 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// Make sure the script runs in editor mode as well as play mode.
+[ExecuteInEditMode]
+
+public class CustomTerrain : MonoBehaviour
+{
+    private void Awake()
+    {
+        // Create a tag manager and add the existing tags to it.
+        SerializedObject tagManager = new SerializedObject(AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0]);
+
+        // Create a tags property which will import all the tags from the tag manager.
+        SerializedProperty tagsProperty = tagManager.FindProperty("tags");
+
+        // Add new tags to the tag manager.
+        AddTag(tagsProperty, "Terrain");
+        AddTag(tagsProperty, "Cloud");
+        AddTag(tagsProperty, "Shore");
+
+        // Apply tag changes to tag database.
+        tagManager.ApplyModifiedProperties();
+
+        gameObject.tag = "Terrain";
+
+        return;
+    }
+
+    void AddTag(SerializedProperty tagsProperty, string newTag)
+    {
+        // Ensure the tag doesn't already exist.
+        for (int currentTag = 0; currentTag < tagsProperty.arraySize; currentTag++)
+        {
+            SerializedProperty tag = tagsProperty.GetArrayElementAtIndex(currentTag);
+
+            if (tag.stringValue.Equals(newTag))
+            {
+                return;
+            }
+        }
+
+        tagsProperty.InsertArrayElementAtIndex(0);
+        SerializedProperty newTagProperty = tagsProperty.GetArrayElementAtIndex(0);
+        newTagProperty.stringValue = newTag;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
